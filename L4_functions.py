21a81a47098995d276b0a4d66a21bf7ebe34bb9f
Josh <120067273+kwaneee@users.noreply.github.com>
2025-01-26 20:45:11 -0500

    first commit of files ecor

---
+++ L4_functions.py
@@ -0,0 +1,18 @@
+abs(-3)
+x = -9
+abs(x)
+
+min(10, 5, -10)
+
+round(3.3)
+
+round(3.555,2)
+#to two decimal places
+
+pow(5, 2)
+# is equivalent to
+5 ** 2
+
+
+x = 5
+print(x)
---
+++ L4_importing.py
@@ -0,0 +1,11 @@
+from math import factorial, sqrt
+from math import pi as PI
+x = 3
+y = factorial(x)
+print(y)
+x = sqrt(25)
+print(x)
+print(PI)
+pi = 3.1
+print(pi)
+
